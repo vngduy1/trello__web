7422b3800ddf3e5f74caec14f23b8c6b6ee28c00
Duy <duyvu372001@gmail.com>
2023-11-25 22:33:39 +0900

    commit-f2

---
+++ src/theme.js
@@ -0,0 +1,23 @@
+import { experimental_extendTheme as extendTheme } from "@mui/material/styles";
+import { red } from "@mui/material/colors";
+
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        // primary: {
+        //   main: "#ff5252",
+        // },
+      },
+    },
+    dark: {
+      palette: {
+        // primary: {
+        //   main: "#000",
+        // },
+      },
+    },
+  },
+});
+
+export default theme;
